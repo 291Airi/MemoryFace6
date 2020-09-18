b7369a01d43f2860a571e62a181f67737f1ba3e7
291Airi <loveairi291@gmail.com>
2020-09-18 17:14:12 +0900

    言ってくれた通り、変更しました… だけど私のできなくって… よろしくお願いします！

--- MemoryFace2/ChoiceViewController.swift
+++ MemoryFace2/ChoiceViewController.swift
@@ -8,25 +8,25 @@
 
 import UIKit
 import RealmSwift
-import AssetsLibrary
 
-class ChoiceViewController: UIViewController,UITableViewDelegate{
+class ChoiceViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     var todoItem: Results<personArray>!
-
-    @IBOutlet weak var tableview: UITableView!
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         // 永続化されているデータを取りだす
-
+        
         let persons = List<personArray>()
-
-
+        
         do{
             let realm = try Realm()
             todoItem = realm.objects(personArray.self)
+            
             //tableView.reloadData()
             super.viewDidLoad()
         }catch{
@@ -45,22 +45,29 @@ class ChoiceViewController: UIViewController,UITableViewDelegate{
         super.didReceiveMemoryWarning()
     }
 
+   
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int {
         return todoItem.count
+ 
     }
 
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexpath: NSIndexPath)->UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+        print("ccccc")
+        
 
-        let object = todoItem[indexpath.row]
+              let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+             
+            
+              let object = todoItem[indexPath.row]
 
-        cell.textLabel?.text = object.textFieldString
-        return cell
-    }
-
-    // TableViewのCellの削除を行った際に、Realmに保存したデータを削除する
+              cell.textLabel?.text = object.textFieldString
+              return cell
+ 
+          }
+// TableViewのCellの削除を行った際に、Realmに保存したデータを削除する
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCell.EditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-
 
         if(editingStyle == UITableViewCell.EditingStyle.delete) {
             do{
@@ -72,102 +79,6 @@ class ChoiceViewController: UIViewController,UITableViewDelegate{
             }catch{
             }
             tableView.reloadData()
+        }
     }
-}
-
-
-
-
-
-
-//import UIKit
-//import RealmSwift
-//
-//class ChoiceViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
-//
-//    private var realm: Realm!
-//
-//    @IBOutlet weak var table: UITableView!
-//
-//    //名前を入れるための配列
-//    var NameArray = [String]()
-//
-//    //写真を入れるための配列
-//    var imageNameArray = [String]()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        table.dataSource = self
-//
-//        table.delegate = self
-//
-//        // (1)Realmのインスタンスを生成する
-//               let realm2 = try! Realm()
-//        // (2)全データの取得
-//               let results = realm2.objects(personArray.self)
-//        // (3)取得データの確認
-//               print(results)
-//
-//
-//       //NameArray,imageNameArrayに名前を入れる
-//        NameArray = [results[0].textFieldString]
-//        imageNameArray = [results[0].pictureurl]
-//
-//    }
-//
-//    //セルの数を設定
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int{
-//        return NameArray.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-//
-//        //セルに画像を表示する
-//        cell?.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
-//
-//        //セルにNameArrayを表示する
-//        cell?.textLabel?.text = NameArray[indexPath.row]
-//
-//        return cell!
-//    }
-//
-//    //セルが押された時に呼ばれるメソッド
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//            print("\(NameArray[indexPath.row])が選ばれました")
-//        // (1)Realmのインスタンスを生成する
-//            let realm2 = try! Realm()
-//        // (2)全データの取得
-//            let results = realm2.objects(personArray.self)
-//        // (3)取得データの確認
-//            print(results)
-//        try! realm.write {
-//            realm.delete(results)
-//            // (1)Realmのインスタンスを生成する
-//                let realm2 = try! Realm()
-//            // (2)全データの取得
-//                let results = realm2.objects(personArray.self)
-//            // (3)取得データの確認
-//                print(results)
-//
-//        }
-//
-//
-//        //スワイプしたセルを削除
-//        if editingStyle == UITableViewCell.EditingStyle.delete {
-//            NameArray.remove(at: indexPath.row)
-//            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
-//        }
-//
-//
-//    }
-//
-//    //セルの編集許可
-//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool{
-//        return true
-//    }
-//
-//
-    
 }
